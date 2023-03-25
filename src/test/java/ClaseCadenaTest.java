0e6dc043420ae91c3f7e8ccab04b64915d7c9e0b
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-03-25 20:48:24 +0100

    finalizacion de tests

--- src/test/java/ClaseCadenaTest.java
+++ src/test/java/ClaseCadenaTest.java
@@ -5,5 +5,12 @@ class ClaseCadenaTest {
     @org.junit.jupiter.api.Test
     void devuelveIniciales() {
         String prueba1 = ClaseCadena.DevuelveIniciales("Eloy Sánchez Tamayo");
+        assertEquals("E.S.T.", prueba1);
+        String prueba2 = ClaseCadena.DevuelveIniciales("Eloy Sánchez");
+        assertEquals("E.S.", prueba2);
+        String prueba3 = ClaseCadena.DevuelveIniciales("");
+        assertEquals("", prueba3);
+        String prueba4 = ClaseCadena.DevuelveIniciales(null);
+        assertEquals("", prueba4);
     }
 }
